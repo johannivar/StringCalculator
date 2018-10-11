315ba5b1d1619fab79c5fe273e2ad9dc8936daba
Jóhann Ívar Björnsson <johann.b@agrdynamics.com>
2018-10-11 23:58:29 +0000

    add function refactored, deleted console.log commands used for developing

--- calculator.js
+++ calculator.js
@@ -11,37 +11,22 @@ function add(numbers) {
     //console.log("numberArray has length " + numberArray.length);
     var sum = 0;
     for(i = 0; i < numberArray.length; i++) {
-      console.log("number: '" + numberArray[i] + "'");
-
       if(numberArray[i].includes("\n")) 
       {
-        console.log("number " + numberArray[i] + " includes newline");
-        console.log("'" + numbers[i] + "' includes newline");
         var newLineNumbers = numberArray[i].split("\n");
-        console.log("newLineNumbers = " + newLineNumbers);
         for(j = 0; j < newLineNumbers.length; j++) {
-          console.log("adding" + newLineNumbers[j]);
           sum += parseInt(newLineNumbers[j]);
         }
       }
       else
       {
-        console.log("adding" + numberArray[i]);
         sum += parseInt(numberArray[i]);
       }
-      
     }
-
     return parseInt(sum);
   }
-
   return parseInt(numbers);
-  
-  
 }
-
-var sum = add("1\n2,3");
-console.log("sum = " + sum);
 
 module.exports = add;
 
