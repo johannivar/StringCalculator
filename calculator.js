3e4f1aa69d5487635f516b9535bd084a8457f838
Jóhann Ívar Björnsson <johann.b@agrdynamics.com>
2018-10-11 23:11:44 +0000

    Step 1 complete, add function can take 0, 1 or 2 numbers as a string and return their sum

--- calculator.js
+++ calculator.js
@@ -5,6 +5,12 @@ function add(numbers) {
   if(numbers == "") 
     return 0;
   
+  if(numbers.includes(","))
+  {
+    var numberArray = numbers.split(",");
+    return parseInt(numberArray[0]) + parseInt(numberArray[1]);
+  }
+
   return parseInt(numbers);
   
   
--- calculator.test.js
+++ calculator.test.js
@@ -10,3 +10,8 @@ it("should return 0", () => {
 it("should return number when only one number is in the string", () => {
   expect(add("5")).toBe(5);
 });
+
+it("should return sum of two given numbers", () => {
+  expect(add("1,2")).toBe(3);
+});
+
