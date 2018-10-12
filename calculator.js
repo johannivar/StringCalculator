5c2a5f34e1e2199d350556222f3cbeb23bbaa66a
Jóhann Ívar Björnsson <johann.b@agrdynamics.com>
2018-10-12 00:21:52 +0000

    Step 3 improved, did not accept only newline symbols

--- calculator.js
+++ calculator.js
@@ -2,14 +2,16 @@
 
 //add function
 function add(numbers) {
+  console.log("testing: " + numbers);
   if(numbers == "") 
     return 0;
   
+  var sum = 0;
+
   if(numbers.includes(","))
   {
     var numberArray = numbers.split(",");
     //console.log("numberArray has length " + numberArray.length);
-    var sum = 0;
     for(i = 0; i < numberArray.length; i++) {
       if(numberArray[i].includes("\n")) 
       {
@@ -25,8 +27,24 @@ function add(numbers) {
     }
     return parseInt(sum);
   }
+  else if(numbers.includes("\n")) 
+  {
+    console.log("no commas, but newline symbol");
+    var numberArray = numbers.split("\n");
+    for(i = 0; i < numberArray.length; i++) {
+      console.log("number: '" + numberArray[i] + "'");
+      console.log("adding " + numberArray[i]);
+      sum += parseInt(numberArray[i]);
+    }
+    return parseInt(sum);
+  }
+
+
   return parseInt(numbers);
 }
+
+var sum = (add("1\n2"));
+console.log("sum: " + sum);
 
 module.exports = add;
 
--- calculator.test.js
+++ calculator.test.js
@@ -20,7 +20,11 @@ it("should summarize multiple numbers", () => {
   expect(add("1,2,3")).toBe(6);
 });
 
+it("should accept only newline as delimitors", () => {
+  expect(add("1\n2")).toBe(3);
+})
 
-it("should accept both ',' or '\n' as delimitors", () => {
+
+it("should accept both ',' or newline as delimitors", () => {
   expect(add("1\n2,3")).toBe(6);
 }) 
